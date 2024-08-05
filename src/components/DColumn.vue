ab4a2f85b37d2a7eb60532caa8bab99c04d8996e
mjsong07 <182254440@qq.com>
2024-08-05 09:56:49 +0800

    perf: 优化代码结构，调整为函数式编程

--- src/components/DColumn.vue
+++ src/components/DColumn.vue
@@ -1,24 +1,25 @@
-<template> 
-  <div :class="{isError: props.row.errorList && props.row.errorList.includes(props.property)}">  
+<template>
+  <div :class="{isError: props.row.errorList && props.row.errorList.includes(props.property)}">
     <RenderContent />
-  </div> 
+  </div>
 </template>
 
 <script setup lang="tsx">
-import { ElSelect, ElOption, ElInput, ElText, ElDatePicker, ElSelectV2 } from 'element-plus'; 
+import { ElSelect, ElOption, ElInput, ElText, ElDatePicker, ElSelectV2 } from 'element-plus';
+import { JSX } from 'vue/jsx-runtime';
 
 interface Option {
   value: number | string;
   label: string;
 }
 
-  // 定义了表格列配置的属性类型
+// 定义了表格列配置的属性类型
 interface Props {
   column: any, // 表格列的原始数据对象
   type: 'select' | 'input' | 'date'; // 表格列的输入类型，可以是选择框、输入框或者日期选择器
   label: string; // 表格列的标签
   property: string; // 表格列的属性名
-  render?: 'component' | 'slot'; // 可选的自定义渲染方式， 不传则按员el-table的方式渲染
+  render?: 'component' | 'slot'; // 可选的自定义渲染方式，不传则按员el-table的方式渲染
   options?: Array<Option> | Function; // 选择项数据源，可以是选项数组或者一个返回选项数组的函数
   row: any; // 当前行的数据对象
   modelValue: any; // 当前列的绑定值
@@ -80,7 +81,7 @@ const onValChange = (value: any) => {
 };
 
 const onValNumberChange = (value: any) => {
-  console.log("onValNumberChange",value)
+  console.log("onValNumberChange", value)
   value = processValue(value, props);
   emits('update:modelValue', value);
 };
@@ -98,95 +99,101 @@ const getOptions = (props: Props) => {
   return props.options || [];
 };
 
+const renderSelect = (nowVal: any, readOnly: boolean, options: Option[]) => {
+  if (readOnly) {
+    const selectedOption = options.find((o: any) => {
+      const valueKey = props.attr?.optionValue || 'value';
+      return o[valueKey] == nowVal;
+    });
+    const label = selectedOption?.label || (props.attr?.allowCreate ? nowVal : '');
+    return <ElText>{label}</ElText>;
+  }
+
+  const selectOptions = options.map((option: Option) => ({
+    label: props.attr?.optionlabel ? option[props.attr.optionlabel] : option.label,
+    value: props.attr?.optionValue ? option[props.attr.optionValue] : option.value,
+  }));
+
+  return options.length > 1000 ? (
+    <ElSelectV2
+      v-model={props.row[props.property]}
+      options={selectOptions}
+      onChange={onValChange}
+      allow-create={props.attr?.allowCreate}
+      filterable
+      clearable
+    />
+  ) : (
+    <ElSelect
+      v-model={props.row[props.property]}
+      onChange={onValChange}
+      allow-create={props.attr?.allowCreate}
+      default-first-option={props.attr?.allowCreate}
+      filterable
+      clearable
+    >
+      {selectOptions.map((option) => (
+        <ElOption label={option.label} value={option.value} />
+      ))}
+    </ElSelect>
+  );
+};
+
+const renderDate = (nowVal: any, readOnly: boolean) => {
+  return readOnly ? (
+    <ElText>{nowVal}</ElText>
+  ) : (
+    <ElDatePicker
+      v-model={props.row[props.property]}
+      format={props.attr?.format}
+      value-format={props.attr?.valueFormat}
+      type={props.attr?.type}
+    />
+  );
+};
+
+const renderInput = (nowVal: any, readOnly: boolean) => {
+  const formatVal = props.attr?.type === 'number'
+    ? formatNum(nowVal, props.attr?.precision || 0)
+    : nowVal;
+
+  return readOnly ? (
+    <ElText>{formatVal}</ElText>
+  ) : (
+    <ElInput
+      maxlength={props.attr?.maxlength || Infinity}
+      v-model={props.row[props.property]}
+      onInput={(value: any) => {
+        if (props.attr?.type === 'number') {
+          onValNumberChange(value);
+        } else {
+          onValChange(value);
+        }
+      }}
+    />
+  );
+};
+
 const RenderContent = () => {
   const nowVal = props.row[props.property];
   const readOnly = isReadOnly(props);
   const options = getOptions(props);
 
-  if (props.type === 'select') {
-    if (readOnly) {
-      const selectedOption = options.find((o: any) => {
-        const valueKey = props.attr?.optionValue || 'value';
-        return o[valueKey] == nowVal;
-      });
-      const label = selectedOption?.label || (props.attr?.allowCreate ? nowVal : '');
-      return <ElText>{label}</ElText>;
-    }
+  const renderMap: { [key: string]: () => JSX.Element } = {
+    select: () => renderSelect(nowVal, readOnly, options),
+    date: () => renderDate(nowVal, readOnly),
+    input: () => renderInput(nowVal, readOnly),
+  };
 
-    const selectOptions = options.map((option: Option) => ({
-      label: props.attr?.optionlabel ? option[props.attr.optionlabel] : option.label,
-      value: props.attr?.optionValue ? option[props.attr.optionValue] : option.value,
-    }));
-
-    return options.length > 1000 ? (
-      <ElSelectV2
-        v-model={props.row[props.property]}
-        options={selectOptions}
-        onChange={onValChange}
-        allow-create={props.attr?.allowCreate}
-        filterable
-        clearable
-      />
-    ) : (
-      <ElSelect
-        v-model={props.row[props.property]}
-        onChange={onValChange}
-        allow-create={props.attr?.allowCreate}
-        default-first-option={props.attr?.allowCreate}
-        filterable
-        clearable
-      >
-        {selectOptions.map((option) => (
-          <ElOption label={option.label} value={option.value} />
-        ))}
-      </ElSelect>
-    );
-  }
-
-  if (props.type === 'date') {
-    return readOnly ? (
-      <ElText>{nowVal}</ElText>
-    ) : (
-      <ElDatePicker
-        v-model={props.row[props.property]}
-        format={props.attr?.format}
-        value-format={props.attr?.valueFormat}
-        type={props.attr?.type}
-      />
-    );
-  }
-
-  if (props.type === 'input') {
-    const formatVal = props.attr?.type === 'number'
-      ? formatNum(nowVal, props.attr?.precision || 0)
-      : nowVal;
-
-    return readOnly ? (
-      <ElText>{formatVal}</ElText>
-    ) : (
-      <ElInput
-        maxlength={props.attr?.maxlength || Infinity}
-        v-model={props.row[props.property]}
-        onInput={(value: any) => {
-          if (props.attr?.type === 'number') {
-            onValNumberChange(value,props);
-          } else {
-            onValChange(value);
-          }
-        }}
-      />
-    );
-  }
-
-  return <ElText>{nowVal}</ElText>;
+  return renderMap[props.type] ? renderMap[props.type]() : <ElText>{nowVal}</ElText>;
 };
 
-const formatNum = (num:string,fixedDigit:number = 0) => { 
-  if(num === undefined) {
-    return ''
+const formatNum = (num: string, fixedDigit: number = 0) => {
+  if (num === undefined) {
+    return '';
   }
-  let numFixed = Number(num).toFixed(fixedDigit)
-  let formatNum = `${numFixed}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-  return `${formatNum}`
-}
+  let numFixed = Number(num).toFixed(fixedDigit);
+  let formatNum = `${numFixed}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return `${formatNum}`;
+};
 </script>
